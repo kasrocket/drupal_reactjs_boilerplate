9be7f4113e20977a8c265c51e5f6a78ae3b4cff5
kas <kas@devbox.local>
2020-07-23 12:53:04 +0200

    content

---
+++ reactjs/components/01_atoms/MainMenu/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from '../../../routes';
+
+const MainMenu = () => (
+  <li>
+    <Link to="/">
+      <a className="nav-link">Home</a>
+    </Link>
+  </li>
+);
+
+export default MainMenu;
---
+++ reactjs/components/03_organisms/GlobalFooter/index.js
@@ -0,0 +1,6 @@
+import './_style.scss';
+import React from 'react';
+
+const GlobalFooter = () => <footer>Some example footer content test.</footer>;
+
+export default GlobalFooter;
